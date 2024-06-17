52e9137eaa7dcd43d3ac24be34dbd8be9719217d
Mohammed Ameen <ameerapex496@gmail.com>
2024-06-17 22:15:33 +0200

    added styling and more changes and all buttons

--- src/app/home/component/landingPage.tsx
+++ src/app/home/component/landingPage.tsx
@@ -3,11 +3,12 @@ import {
   faChevronRight,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Image from "next/image";
 
 export default function LandingPage() {
   return (
-    <div className="relative flex h-[600px] w-[100%] flex-col ">
-      <div className="absolute top-[50%] flex h-[40px] w-[100%] flex-row items-center justify-between p-[20px] text-center text-white">
+    <div className="relative flex h-[600px] flex-col overflow-hidden ">
+      <div className="absolute  top-[50%] z-10 flex h-[40px] w-[100%] flex-row items-center justify-between p-[20px] text-center text-white">
         <div className=" item-center flex cursor-pointer justify-center rounded-[50%] bg-slate-300 p-[6px] text-center hover:bg-[#fe6501]">
           <FontAwesomeIcon
             icon={faChevronLeft}
@@ -21,8 +22,16 @@ export default function LandingPage() {
           />
         </div>
       </div>
-      <div></div>
-      <div className="absolute bottom-[15px] flex w-[100%] flex-row items-center justify-center gap-[10px] text-center">
+
+      <div className=" absolute -z-10 flex h-[100%] w-[100%] flex-row items-center justify-center text-center">
+        <img
+          src="https://via.placeholder.com/1980x1080"
+          alt="Your image description"
+          className=" h-[100%] w-[100%] bg-no-repeat object-cover"
+        />
+      </div>
+
+      <div className=" absolute bottom-[15px] flex w-[100%] flex-row items-center justify-center gap-[10px] text-center">
         <div className="relative h-[12px] w-[40px] cursor-pointer border-[3px] border-[#fe6501] bg-transparent hover:border-[#665bdb]"></div>
         <div className="relative h-[12px] w-[40px] cursor-pointer border-[3px] border-[#fe6501] bg-transparent hover:border-[#665bdb]"></div>
         <div className="relative h-[12px] w-[40px] cursor-pointer border-[3px] border-[#fe6501] bg-transparent hover:border-[#665bdb]"></div>
