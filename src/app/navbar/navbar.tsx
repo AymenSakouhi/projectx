a8f3b268539ff79d0e5e5b952a4f658d3cd556fa
Mohammed Ameen <ameerapex496@gmail.com>
2024-06-17 21:36:05 +0200

    finished the navbar

--- src/app/navbar/navbar.tsx
+++ src/app/navbar/navbar.tsx
@@ -3,11 +3,15 @@ import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import Logo from "public/logo_header.png";
 import Image from "next/image";
-import homeIcon from "public/icons8-home-120.png";
-import blogIcon from "public/icons8-blog-dotted-96.png";
-import pageIcon from "public/icons8-page-96.png";
-import shortIcon from "public/icons8-chemical-96.png";
-import contactIcon from "public/icons8-contact-us-96.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faBlog,
+  faContactCard,
+  faCut,
+  faHome,
+  faPager,
+} from "@fortawesome/free-solid-svg-icons";
 
 type navType = {
   name: string;
@@ -19,32 +23,33 @@ const navData: navType[] = [
   {
     name: "Home",
     path: "/",
-    icon: homeIcon,
+    icon: faHome,
   },
   {
     name: "Pages",
     path: "/pages",
-    icon: pageIcon,
+    icon: faPager,
   },
   {
     name: "Blog",
     path: "/blog",
-    icon: blogIcon,
+    icon: faBlog,
   },
   {
     name: "Shortcode",
     path: "/shortcode",
-    icon: shortIcon,
+    icon: faCut,
   },
   {
     name: "Contact",
     path: "/contact",
-    icon: contactIcon,
+    icon: faContactCard,
   },
 ];
 
 export default function Navbar() {
-  const [isLaptop, setIsLaptop] = useState(false);
+  const [isLaptop, setIsLaptop] = useState(1024);
+  const [menuheight, setMenuheight] = useState(40);
 
   useEffect(() => {
     const updateMedia = () => {
@@ -66,11 +71,13 @@ export default function Navbar() {
   return (
     <div className="flex w-[100%] items-center justify-center bg-[#ffffff] text-center phone:h-auto phone:flex-col laptop:h-[126px] laptop:flex-row ">
       <div className="flex h-[100%] flex-col items-center justify-center text-center phone:w-[100%] laptop:w-[50%]">
-        <Image
-          className=" h-[100%]  w-[100px] tablet:h-[100%] tablet:w-[189px]"
-          src={Logo}
-          alt=""
-        />
+        <Link href="/">
+          <Image
+            className=" h-[100%]  w-[100px] tablet:h-[100%] tablet:w-[189px]"
+            src={Logo}
+            alt=""
+          />
+        </Link>
       </div>
       <div className="flex h-[100%] flex-row items-center text-center phone:w-[100%] phone:justify-center laptop:w-[50%] laptop:justify-start">
         {isLaptop ? (
@@ -79,28 +86,34 @@ export default function Navbar() {
               <Link
                 key={index}
                 href={nav.path}
-                className=" flex h-[100%] flex-col items-center justify-center text-center font-light leading-[24px] text-[#665bdb] phone:text-[13px]  tablet:text-[14px] laptop:p-[7px] laptop:pb-[20px]  laptop:pt-[25px] laptop:text-[15px]  desktop:p-[10px] desktop:pb-[25px]  desktop:pt-[30px] desktop:text-[18px]"
+                className=" flex h-[100%] flex-col items-center justify-center text-center font-light leading-[24px] text-[#665bdb] hover:bg-[#fe6501] hover:text-white phone:text-[13px]  tablet:text-[14px] laptop:p-[7px] laptop:pb-[20px]  laptop:pt-[25px] laptop:text-[15px]  desktop:p-[10px] desktop:pb-[25px]  desktop:pt-[30px] desktop:text-[18px]"
               >
+                <FontAwesomeIcon
+                  className="color-[#665bdb] hover:color-white"
+                  style={{ height: "40px", width: "40px" }}
+                  icon={nav.icon}
+                />
+
                 <p className="mb-[15px] mt-[0px] flex flex-col gap-[5px] phone:m-[12px] laptop:m-[19px] ">
-                  <Image
-                    src={nav.icon}
-                    alt="navIcon"
-                    style={{ height: "100%" }}
-                  />
-                  {nav.name}00
+                  {nav.name}
                 </p>
               </Link>
             );
           })
         ) : (
-          <div className="h-[42px] w-[90%]  border border-slate-300 p-[10px] pl-[20px] pr-[20px]">
-            <div className=" flex h-[110%] w-[100%] flex-row justify-between ">
-              <p className="text-[#665bdb]">menu</p>
-              <div className="flex h-[100%] w-[30px] flex-col  items-center justify-center gap-[7px] text-center">
-                <hr className="m-0 h-[8px] w-[100%] bg-[#665bdb] p-0" />
-                <hr className="m-0 h-[8px] w-[100%] bg-[#665bdb] p-0" />
-                <hr className="m-0 h-[8px] w-[100%] bg-[#665bdb] p-0" />
-              </div>
+          <div
+            className="mb-[20px] mt-[20px] h-[42px] w-[90%] overflow-hidden border border-slate-300 "
+            style={{ height: `${menuheight}px` }}
+            onClick={() => {
+              menuheight == 280 ? setMenuheight(40) : setMenuheight(280);
+            }}
+          >
+            <div className=" flex h-[40px] w-[100%] cursor-pointer flex-row items-center justify-between pl-[15px] pr-[15px]  text-center text-[#665bdb] hover:bg-[#fe6501] hover:text-white">
+              <p>Menu</p>
+              <FontAwesomeIcon
+                style={{ height: "25px", width: "25px" }}
+                icon={faBars}
+              />
             </div>
             <div>
               {navData.map((nav: navType, index) => {
@@ -108,11 +121,9 @@ export default function Navbar() {
                   <Link
                     key={index}
                     href={nav.path}
-                    className=" flex h-[100%] flex-col items-center justify-center text-center font-light leading-[24px] text-[#665bdb] phone:text-[13px]  tablet:text-[14px] laptop:p-[7px] laptop:pb-[20px]  laptop:pt-[25px] laptop:text-[15px]  desktop:p-[10px] desktop:pb-[25px]  desktop:pt-[30px] desktop:text-[18px]"
+                    className=" flex h-[100%] flex-col items-start justify-start text-start font-light leading-[24px] text-[#665bdb] hover:bg-[#fe6501] hover:text-white phone:text-[13px]  tablet:text-[14px] laptop:p-[7px] laptop:pb-[20px]  laptop:pt-[25px] laptop:text-[15px]  desktop:p-[10px] desktop:pb-[25px]  desktop:pt-[30px] desktop:text-[18px]"
                   >
                     <p className="mb-[15px] mt-[0px] phone:m-[12px] laptop:m-[19px] ">
-                      <Image src={nav.icon} alt="navIcon" />
-
                       {nav.name}
                     </p>
                   </Link>
