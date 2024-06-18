1779a2a67c8c220d356bdbbc68fc8acf3ed76bb9
Mohammed Ameen <ameerapex496@gmail.com>
2024-06-18 21:21:47 +0200

    added ContentTemplate html tree

---
+++ src/app/home/component/contentTemplate.tsx
@@ -0,0 +1,24 @@
+export default function ContentTemplate() {
+  return (
+    <div>
+      <div>
+        <h1>Welcome to Fable</h1>
+        <p>Fable daycare, preschool, and kindergarten</p>
+      </div>
+      <div>this is orange line between</div>
+      <div>
+        <div>
+          <i>this is image or icon</i>
+
+          <h1>Morbi Etos</h1>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor
+            laudantium quo laboriosam temporibus corrupti eos quod fugit animi
+            iure,
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
