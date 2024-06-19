cf95ff3a44a98f5caada6623b6d0409f709e2c75
Mohammed Ameen <ameerapex496@gmail.com>
2024-06-19 21:02:11 +0200

    added more logic and more styling

--- src/app/home/component/overViewPage.tsx
+++ src/app/home/component/overViewPage.tsx
@@ -1,16 +1,25 @@
-export default function overViewPage() {
+export default function OverViewPage() {
   return (
-    <div>
-      <div>
-        <div>
-          <div></div>
+    <div className="flex h-[564px] flex-row items-center justify-center bg-[#f5f5f5f5] text-center">
+      <div className="flex h-[80%] w-[40%] flex-col items-center justify-center text-center">
+        <div className="flex w-[100%] flex-row items-center justify-center gap-[19px] text-center">
+          <button
+            type="button"
+            className=" after:border-bottom after:content h-[50px] w-[140px] bg-[#fe6501] p-[15px] pb-[12px] pt-[12px] font-openSans text-[18px] font-normal leading-[27px] text-[rgb(255,255,255)] after:absolute after:border-b-[50px] after:border-white"
+          >
+            OverView
+          </button>
+          <h1 className=" h-[50px] w-[140px]  p-[15px]  pb-[12px] pt-[12px] font-openSans text-[18px] font-normal leading-[27px] text-[rgb(255,255,255)] hover:bg-[#fe6501]">
+            Our Mission
+          </h1>
         </div>
+        <div>here comes the overview and mission page </div>
       </div>
-      <div>
+      <div className="flex h-[80%] w-[40%] flex-col items-center justify-center text-center">
         <div>
-          <div></div>
-          <div></div>
+          <div>here will go one big image with two buttons</div>
         </div>
+        <div>here will go 3 images</div>
       </div>
     </div>
   );
--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,14 @@
-import CallToAction from "~/app/home/component/callToAction";
-import LandingPage from "~/app/home/component/landingPage";
-import AboutUs from "~/app/home/component/aboutUs";
+import CallToAction from "@/home/component/callToAction";
+import LandingPage from "@/home/component/landingPage";
+import AboutUs from "@/home/component/aboutUs";
+import OverViewPage from "@/home/component/overViewPage";
 export default function HomePage() {
   return (
     <>
       <LandingPage />
       <CallToAction />
       <AboutUs />
+      <OverViewPage />
     </>
   );
 }
