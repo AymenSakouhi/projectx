71767bb24d9feb1d5692c1c1a8faedce2ca81b9f
Mohammed Ameen <ameerapex496@gmail.com>
2024-06-25 08:16:16 +0200

    added new insightsection with html tree

---
+++ src/app/home/component/insightSection.tsx
@@ -0,0 +1,39 @@
+export default function insightSection() {
+  return (
+    <div>
+      <div>
+        <h1>
+          We have been educating children for over fifteen years. Our goal is to
+          create a place that engages each child.
+        </h1>
+        <p>
+          Praesent arcu gravida vehicula est node maecenas loareet morbi a dosis
+          luctus. Urna eget lacinia eleifend praesent luctus a arcu quis
+          facilisis venenatis aenean interdum.
+        </p>
+        <ul>
+          <li>
+            CHECKED Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </li>
+          <li>
+            CHECKED Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </li>
+          <li>
+            CHECKED Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </li>
+
+          <li>
+            CHECKED Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </li>
+        </ul>
+      </div>
+      <div>
+        <div>
+          <img src="" alt="" />
+          <h1></h1>
+          <p></p>
+        </div>
+      </div>
+    </div>
+  );
+}
