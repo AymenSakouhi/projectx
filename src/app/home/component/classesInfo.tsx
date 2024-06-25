f2a5b709d2209d9125601743ad3aefff64517641
Mohammed Ameen <ameerapex496@gmail.com>
2024-06-25 07:36:18 +0200

    added classes info with response   and more

--- src/app/home/component/classesInfo.tsx
+++ src/app/home/component/classesInfo.tsx
@@ -1,52 +1,127 @@
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
 import tempOrangeStrip from "public/template_content_icons/temp_orange_strip.png";
+
+type classesTypes = {
+  title: string;
+  className: string;
+  monthOld: number[];
+  monthOldText: string;
+  classSize: number;
+  classSizeText: string;
+  classImage: string;
+};
+
+const classesData: classesTypes[] = [
+  {
+    title: "Little Lambs",
+    className: "Class name",
+    monthOld: [12, 24],
+    monthOldText: "Month olds",
+    classSize: 9,
+    classSizeText: "Class Size",
+    classImage: `https://via.placeholder.com/1980x1080`,
+  },
+  {
+    title: "Bouncy Bears",
+    className: "Class name",
+    monthOld: [2, 3],
+    monthOldText: "Month olds",
+    classSize: 12,
+    classSizeText: "Class Size",
+    classImage: `https://via.placeholder.com/1980x1080`,
+  },
+  {
+    title: "Tenderhearts",
+    className: "Class name",
+    monthOld: [3, 4],
+    monthOldText: "Month olds",
+    classSize: 15,
+    classSizeText: "Class Size",
+    classImage: `https://via.placeholder.com/1980x1080`,
+  },
+  {
+    title: "Shining Stars",
+    className: "Class name",
+    monthOld: [2, 3],
+    monthOldText: "Month olds",
+    classSize: 12,
+    classSizeText: "Class Size",
+    classImage: `https://via.placeholder.com/1980x1080`,
+  },
+];
 
 export default function ClassesInfo() {
   return (
     <div className="flex h-auto flex-col items-center justify-center pb-[80px] pt-[60px] text-center">
-      <h1 className=" mb-[15px] font-Lato text-[50px] font-light leading-[50px] text-[#665bdb]">
+      <h1 className=" mb-[15px] font-Lato font-light leading-[50px] text-[#665bdb] phone:text-[40px] laptop:text-[50px]">
         Our Classes
       </h1>
-      <p className="mb-[30px]  pt-[15px] font-openSans font-medium leading-[25px] text-[rgba(150,149,149,0.55)] phone:text-[15px] laptop:text-[18px]">
+      <p className="mb-[30px] pt-[15px] font-openSans font-medium leading-[25px] text-[rgba(150,149,149,0.55)] phone:w-[90%] phone:text-[15px] laptop:w-[100%] laptop:text-[18px]">
         Our preschool program has four dedicated classes
       </p>
       <Image className="mb-[66px]" src={tempOrangeStrip} alt="" />
-      <div className="mb-[60px] flex flex-row items-center justify-center gap-[33px] text-center">
-        <h1 className="font-lato h-[100%] w-[490px] text-start text-[30px] font-normal leading-[36px] text-[#665bdb]">
+      <div className="mb-[60px] flex flex-row flex-wrap items-center justify-center gap-[33px] text-center">
+        <h1 className="font-lato h-[100%] text-start  font-normal leading-[36px] text-[#665bdb] phone:w-[90%] phone:text-[20px] laptop:w-[490px] laptop:text-[30px]">
           We provided four classes with nine to twenty children each aged twelve
           month to five years of age.
         </h1>
-        <p className="h-[100%] w-[490px] text-start font-openSans text-[16px] font-normal leading-[24px] text-[rgba(150,149,149,0.55)]">
+        <p className="h-[100%] text-start font-openSans font-normal leading-[24px] text-[rgba(150,149,149,0.55)] phone:w-[90%] phone:text-[13px] laptop:w-[490px] laptop:text-[16px]">
           Praesent arcu gravida vehicula est node maecenas loareet morbi a dosis
           luctus. Urna eget lacinia eleifend praesent luctus a arcu quis
           facilisis venenatis. Aenean interdum, nibh vitae sodales, magna ante
           feugiat elit maecenas.
         </p>
       </div>
-      <div>
-        <div>
-          <div>
-            <div>
-              <h1>Little Lambs</h1>
-              <p>Class name</p>
-            </div>
-            <div>
-              <div>
-                <h1>3-4</h1>
-                <p>Years olds</p>
+      <div className="flex flex-row flex-wrap items-center justify-center text-center phone:w-[95%] laptop:w-[80%]">
+        {classesData.map((data, index) => {
+          return (
+            <div
+              className=" mb-[30px] flex h-[188px] w-[509px] flex-row items-center justify-center text-center phone:mr-[0] laptop:mr-[32px]"
+              key={index}
+            >
+              <div className=" flex h-[100%] flex-col items-start justify-start bg-[#665bdb] p-[20px] text-start phone:w-[100%] laptop:w-[50%]">
+                <h1 className=" m-[0] p-[0] font-Lato text-[24px] font-normal leading-[29px] text-[#fff]">
+                  Little Lambs
+                </h1>
+                <p className=" font-openSans text-[14px] font-normal leading-[21px] text-[#cccae2]">
+                  Class name
+                </p>
+
+                <div className="mt-[52px] flex h-[50%] w-[100%] flex-row items-center justify-between  text-center  ">
+                  <div className="flex flex-col">
+                    <h1 className=" font-openSans text-[16px] font-normal leading-[24px] text-white">
+                      3-4
+                    </h1>
+                    <p className="font-openSans text-[14px] font-normal leading-[21px] text-[#cccae2]">
+                      Years olds
+                    </p>
+                  </div>
+                  <div className="h-[100%] w-[1px] bg-[rgba(138,138,138,0.74)]"></div>
+                  <div className="flex flex-col">
+                    <h1 className="font-openSans text-[16px] font-normal leading-[24px] text-white">
+                      15
+                    </h1>
+                    <p className="font-openSans text-[14px] font-normal leading-[21px] text-[#cccae2]">
+                      Class size
+                    </p>
+                  </div>
+                </div>
               </div>
-              <img src="" alt="" />
-              <div>
-                <h1>15</h1>
-                <p>Class size</p>
+              <div className=" relative h-[100%] w-[50%]  phone:hidden laptop:flex">
+                <img
+                  className=" absolute z-[-1]  h-[100%] w-[100%] bg-no-repeat object-cover"
+                  src={data.classImage}
+                  alt=""
+                />
+                <button className=" absolute bottom-0 right-0 z-[1] flex flex-row items-center justify-center gap-[5px] bg-[#fe6500] p-[16px] pl-[20px] pr-[20px] text-center font-openSans text-[14px] font-semibold leading-[14px] text-[#fff] hover:bg-[#665bdb]">
+                  CLICK HERE <FontAwesomeIcon icon={faAngleRight} />
+                </button>
               </div>
             </div>
-          </div>
-          <div>
-            <img src="" alt="" />
-            <button>CLICK HERE Icon</button>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
